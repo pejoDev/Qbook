99499ea26deeda4f6240505bf95a27cb1df45dc3
lluksa <leon.luksa@gmail.com>
2021-08-08 21:24:47 +0200

    Added some tests for container component, clean up

---
+++ TIMELOG.md
@@ -0,0 +1,19 @@
+# QBook time log
+
+This file is about project time log.
+
+## Time logging
+
+26.07 - checked out the task, planed project, created angular application => 2.5h
+
+30.07 - created services&models, reading and getting into my-json-server, added db.json with data => 1.5h
+
+31.07 - created components and folder structure, added boostrap => 1h
+
+01.08/02.08 - added html and css for comments, posts and filter component => 3h
+
+03.08/04.08 - connected components to db(posts, users, comments, css/html improvements) => 2h
+
+05.08/06.08 - implemented search, cleanup, code improvement => 2.5h
+
+07.08/08.08 - code improvement, adding some additional features, tests => 3h
--- src/app/feature/posts/posts-overview/container/posts-container.component.spec.ts
+++ src/app/feature/posts/posts-overview/container/posts-container.component.spec.ts
@@ -1,25 +1,57 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PostsFeatureService } from '../posts-feature.service';
+import { postsInitialState } from '../posts-state.model';
+import { PostsOverviewFormService } from '../presentation/posts-search/form/posts-overview-form.service';
+import { IPostsSearchUi } from '../presentation/posts-search/posts-search.ui.model';
 
 import { PostsContainerComponent } from './posts-container.component';
 
 describe('PostsContainerComponent', () => {
-  let component: PostsContainerComponent;
-  let fixture: ComponentFixture<PostsContainerComponent>;
+	let component: PostsContainerComponent;
+	let fixture: ComponentFixture<PostsContainerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PostsContainerComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [PostsContainerComponent],
+			imports: [HttpClientTestingModule, ReactiveFormsModule],
+			providers: [PostsOverviewFormService]
+		}).compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PostsContainerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(PostsContainerComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should search with the form values', () => {
+		const formValue: IPostsSearchUi = {
+			name: 'Leanne Graham',
+			username: 'Bret',
+			email: 'Sincere@april.biz'
+		};
+		component.formGroup.setValue(formValue);
+		const featureService =
+			fixture.debugElement.injector.get(PostsFeatureService);
+		const spyOnFacadeSearch = spyOn(featureService, 'search');
+		component.onSearch();
+		expect(spyOnFacadeSearch).toHaveBeenCalledWith(formValue);
+	});
+
+	it('should reset the form values', () => {
+		const formValue: IPostsSearchUi = {
+			name: 'Leanne Graham',
+			username: 'Bret',
+			email: 'Sincere@april.biz'
+		};
+		component.formGroup.setValue(formValue);
+		component.onReset();
+		expect(component.formGroup.value).toEqual(postsInitialState.searchValues);
+	});
 });
