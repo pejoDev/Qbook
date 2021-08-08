37ffe7808d10202e8f5c40eae568a8193992dfa1
lluksa <leon.luksa@gmail.com>
2021-08-08 22:21:57 +0200

    Added readme, updated db.json

--- src/app/data-access/users/users.service.ts
+++ src/app/data-access/users/users.service.ts
@@ -12,12 +12,12 @@ export class UsersService {
 	 * Function for getting all of available users
 	 */
 	getUsers(): Observable<IUser[]> {
-		return this.httpClient.get<IUser[]>(`${this.url}/user`); //TODO: change for users in db.json
+		return this.httpClient.get<IUser[]>(`${this.url}/users`);
 	}
 	/**
 	 * Function for getting user by user id
 	 */
 	getUserById(userId: number): Observable<IUser> {
-		return this.httpClient.get<IUser>(`${this.url}/user/${userId}`);
+		return this.httpClient.get<IUser>(`${this.url}/users/${userId}`);
 	}
 }
